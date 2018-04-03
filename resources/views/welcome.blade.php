12f7f2c3967305d916da8ee5498cfb150c86b5a3
John Puddephatt <john@jdp.org.uk>
2018-04-03 15:17:50 +0100

    add errors to submit page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,37 +2,51 @@
 
 @section('content')
 
-  <form action="/zXE32nJQ" method="post">
+  <div class="container">
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <form action="/zXE32nJQ" method="post">
 
 
-    <div class="form-group">
-      <label for="name">Name</label>
-      <input class="form-control" type="text" name="name" placeholder="name">
-    </div>
-    <div class="form-group">
-      <label for="email">Email</label>
-      <input class="form-control" type="email" name="email" placeholder="email">
-    </div>
-    <div class="form-group">
-      <label for="name">Subject</label>
-      <input class="form-control" type="text" name="_subject" placeholder="subject">
-    </div>
+      <div class="form-group">
+        <label for="name">Name</label>
+        <input class="form-control" type="text" name="name" placeholder="name">
+      </div>
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input class="form-control" type="email" name="email" placeholder="email">
+      </div>
+      <div class="form-group">
+        <label for="name">Subject</label>
+        <input class="form-control" type="text" name="_subject" placeholder="subject">
+      </div>
 
 
-    <input type="text" name="_honey" value="" style="display:none">
-    <input type="text" name="_redirect" value="http://www.msn.com">
+      <input type="text" name="_honey" value="" style="display:none">
+      <input type="text" name="_redirect" value="http://www.msn.com">
 
-    <input type="file" name="file">
+      <input type="file" name="file">
 
 
-    <div class="form-group">
-      <label for="message">Body</label>
-      <textarea class="form-control" name="message"></textarea>
-    </div>
+      <div class="form-group">
+        <label for="message">Body</label>
+        <textarea class="form-control" name="message"></textarea>
+      </div>
 
-    {!! Captcha::display() !!}
+      {!! Captcha::display() !!}
 
-    <input class="btn btn-primary" type="submit" value="Send" name="submit">
+      <input class="btn btn-primary" type="submit" value="Send" name="submit">
 
-  </form>
+    </form>
+
+  </div>
 @endsection
