bd093c838a1b0bb6f67a8ff72c08d2086ba1a339
John Puddephatt <john@jdp.org.uk>
2019-07-12 16:53:50 +0100

    try and work with arrays better inc pseudo-fieldsets

--- resources/views/emails/email-submission.blade.php
+++ resources/views/emails/email-submission.blade.php
@@ -19,10 +19,10 @@
   @if (is_array($value) && (substr($key, 0, 9) === 'fieldset_'))
     ## {{ $key }}
     @foreach ($value as $key2 => $value2)
-      **{{ $key2 }}:** {{ is_array($value2) ?? implode($value2,', ') : $value2 }}
+      **{{ $key2 }}:** {{ is_array($value2) ? implode($value2,', ') : $value2 }}
     @endforeach
   @else
-    **{{ $key }}:** {{ is_array($value) ?? implode($value,', ') : $value }}
+    **{{ $key }}:** {{ is_array($value) ? implode($value,', ') : $value }}
   @endif
 
 @endforeach
